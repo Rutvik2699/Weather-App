b586ebb326387f81814bb8a5fc5743cbb7a3e3e6
Rutvik2699 <rutvikbhoj2611@gmail.com>
2022-11-05 09:53:08 -0400

    Logic Completed and ready to deploy

--- logic.js
+++ logic.js
@@ -3,8 +3,8 @@ const dateEl = document.getElementById('date');
 const currentWeatherItemsEl= document.getElementById('current-weather-items');
 const timezone = document.getElementById('time-zone');
 const countryEl = document.getElementById('country');
-const weatherForecastEl = document.getElementById('weather-foreacast');
-const currentForecastEl = document.getElementById('current-temp');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
 const days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec',]
 
@@ -20,7 +20,7 @@ setInterval(()=>{
     const minutes = time.getMinutes();
     const ampm = hour>=12?'PM':'AM'
 
-    timeEl.innerHTML= hoursIn12HrFormat + ':'+minutes+`<span id="am-pm">${ampm}</span>`
+    timeEl.innerHTML= (hoursIn12HrFormat<10? '0'+hoursIn12HrFormat:hoursIn12HrFormat) + ':'+(minutes<10?'0'+minutes:minutes)+' '+`<span id="am-pm">${ampm}</span>`
 
 
     dateEl.innerHTML = days[day]+', '+date+' '+months[month]
@@ -37,7 +37,7 @@ function getWeatherData(){
 
         let {latitude,longitude}=success.coords;
 
-        fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&cnt=7&units=metric&appid=${API_KEY}`).then(res=>res.json()).then(data=>{
+        fetch(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&cnt=8&units=metric&appid=${API_KEY}`).then(res=>res.json()).then(data=>{
             console.log(data)
             showWeatherData(data);
         })
@@ -45,9 +45,14 @@ function getWeatherData(){
 }
 
 function showWeatherData(data){
-    let {humidity,pressure}=data.list;
-    let {speed}=data.list;
-    let {sunrise,sunset}=data.list;
+    let {humidity,pressure}=data.list[0];
+    let {speed}=data.list[0];
+    let {sunrise,sunset}=data.list[0];
+
+    timezone.innerHTML=data.city.name;
+    countryEl.innerHTML=data.city.country;
+
+
     currentWeatherItemsEl.innerHTML=`<div class="weather-item">
     <div>Humidity</div>
     <div>${humidity}%</div>
@@ -72,16 +77,28 @@ function showWeatherData(data){
     let otherDayForecast = ''
     data.list.forEach((day,idx)=>{
         if(idx == 0){
-
+            currentTempEl.innerHTML=`
+            <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+            <div class="other">
+                <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
+                <div class="temp">Night - ${day.temp.night}&#176;C</div>
+                <div class="temp">Day - ${day.temp.day}&#176;C</div>
+            </div>
+            `
         }else{
-            otherDayForecast+=`<div class="weather-forecast-item">
+            otherDayForecast += `
+            <div class="weather-forecast-item">
             <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
-            <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="weather icon" class="w-icon">
-            <div class="temp">Night - 25.6&#176;C</div>
-            <div class="temp">Day - 25.6&#176;C</div>
+            <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+            <div class="temp">Night - ${day.temp.night}&#176;C</div>
+            <div class="temp">Day - ${day.temp.day}&#176;C</div>
         </div>`
             
         }
     })
+
+
+    weatherForecastEl.innerHTML=otherDayForecast;
+
 
 }
